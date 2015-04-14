6370e2c1c24f5b8d133d808b0c39c5d31dc4df76
Syed I.R <syed@lukonet.com>
2015-04-14 18:28:22 +0530

    Update Installer

--- phpci-installer.sh
+++ phpci-installer.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #------------------------------------------------------------------------------
 #                       PHPCI Installer for Laravel Homestead
@@ -22,45 +22,31 @@ MYSQL_CONF=~/.my.cnf
 #
 # Helper function to output in color
 #
-function coloredEcho(){
-    local exp=$1;
-    local color=$2;
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-       case $(echo $color | tr '[:upper:]' '[:lower:]') in
-        black) color=0 ;;
-        red) color=1 ;;
-        green) color=2 ;;
-        yellow) color=3 ;;
-        blue) color=4 ;;
-        magenta) color=5 ;;
-        cyan) color=6 ;;
-        white|*) color=7 ;; # white or invalid color
-       esac
-    fi
-    tput setaf $color;
-    echo "$exp";
+coloredEcho(){
+    tput setaf $2;
+    echo "$1";
     tput sgr0;
 }
 
-coloredEcho "****************************" green
-coloredEcho " Welcome to PHPCI Installer" green
-coloredEcho "****************************" green
+coloredEcho "****************************" 2
+coloredEcho " Welcome to PHPCI Installer" 2
+coloredEcho "****************************" 2
 
 # Download PHPCI & Run Composer Install
-coloredEcho "Downloading and Installing PHPCI Dependencies." green
+coloredEcho "Downloading and Installing PHPCI Dependencies." 2
 composer create-project block8/phpci $DEFAULT_DIRNAME --keep-vcs --no-dev --prefer-dist
 cd $DEFAULT_DIRNAME && composer install --no-dev --prefer-dist --optimize-autoloader
 
 # Increase PHP Memory Limit
-coloredEcho "Increasing PHP Memory Limit." green
+coloredEcho "Increasing PHP Memory Limit." 2
 sudo sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php5/cli/php.ini
 
 # Setup Database
-coloredEcho "Setting up MySQL Database." green
+coloredEcho "Setting up MySQL Database." 2
 mysql -u$DB_USER -p$DB_PASSWORD -e "DROP DATABASE IF EXISTS \`${DB_NAME}\`; CREATE DATABASE \`${DB_NAME}\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
 sudo service mysql restart
 
-coloredEcho "Adding MySQL Config." green
+coloredEcho "Adding MySQL Config." 2
 touch $MYSQL_CONF
 cat >$MYSQL_CONF <<EOF
 [mysqld]
@@ -70,22 +56,22 @@ innodb_log_buffer_size=768M
 EOF
 
 # Run PHPCI Install
-coloredEcho "Starting PHPCI Console Install." green
+coloredEcho "Starting PHPCI Console Install." 2
 php ./console phpci:install --url=http://$DEFAULT_DOMAIN --db-host=$DB_HOST --db-name=$DB_NAME --db-user=$DB_USER --db-pass=$DB_PASSWORD
 
 # Setup Cron
-coloredEcho "Setting up Cron Job!" green
+coloredEcho "Setting up Cron Job!" 2
 croncmd="php $(pwd)/console phpci:run-builds"
 cronjob="* * * * * $croncmd"
-cat <(fgrep -i -v "$croncmd" <(crontab -l)) <(echo "$cronjob") | crontab -
+cat <(fgrep -i -v "${croncmd}" <(crontab -l)) <(echo "${cronjob}") | crontab -
 
 # Provision
-coloredEcho "Provisioning PHPCI" green
+coloredEcho "Provisioning PHPCI" 2
 sudo bash /vagrant/scripts/serve.sh $DEFAULT_DOMAIN $(pwd)/public
 
-coloredEcho "*********************" green
-coloredEcho "Install success." green
-coloredEcho "You can access it at:" green
-coloredEcho "http://${DEFAULT_DOMAIN}" yellow
-coloredEcho "*********************" green
-coloredEcho "Don't forget to add the domain to /etc/hosts file on your main machine!" green
+coloredEcho "*********************" 2
+coloredEcho "Install success." 2
+coloredEcho "You can access it at:" 2
+coloredEcho "http://${DEFAULT_DOMAIN}" 3
+coloredEcho "*********************" 2
+coloredEcho "Don't forget to add the domain to /etc/hosts file on your main machine!" 2
