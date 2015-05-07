f008186c1f9bad1e4a62b4b30b6543ad23277f49
Syed I.R <syed@lukonet.com>
2015-05-07 17:00:35 +0530

    Optimise Print Function.

--- phpci-installer.sh
+++ phpci-installer.sh
@@ -22,31 +22,40 @@ MYSQL_CONF=~/.my.cnf
 #
 # Helper function to output in color
 #
-coloredEcho(){
+coloredEcho() {
     tput setaf $2;
     echo "$1";
     tput sgr0;
 }
 
-coloredEcho "****************************" 2
-coloredEcho " Welcome to PHPCI Installer" 2
-coloredEcho "****************************" 2
+info() {
+    coloredEcho "$1" 2;
+}
+
+note() {
+    coloredEcho "$1" 3;
+}
+
+
+info "****************************"
+info " Welcome to PHPCI Installer"
+info "****************************"
 
 # Download PHPCI & Run Composer Install
-coloredEcho "Downloading and Installing PHPCI Dependencies." 2
+info "Downloading and Installing PHPCI Dependencies."
 composer create-project block8/phpci $DEFAULT_DIRNAME --keep-vcs --no-dev --prefer-dist
 cd $DEFAULT_DIRNAME && composer install --no-dev --prefer-dist --optimize-autoloader
 
 # Increase PHP Memory Limit
-coloredEcho "Increasing PHP Memory Limit." 2
+info "Increasing PHP Memory Limit."
 sudo sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php5/cli/php.ini
 
 # Setup Database
-coloredEcho "Setting up MySQL Database." 2
+info "Setting up MySQL Database."
 mysql -u$DB_USER -p$DB_PASSWORD -e "DROP DATABASE IF EXISTS \`${DB_NAME}\`; CREATE DATABASE \`${DB_NAME}\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
 sudo service mysql restart
 
-coloredEcho "Adding MySQL Config." 2
+info "Adding MySQL Config."
 touch $MYSQL_CONF
 cat >$MYSQL_CONF <<EOF
 [mysqld]
@@ -56,22 +65,22 @@ innodb_log_buffer_size=768M
 EOF
 
 # Run PHPCI Install
-coloredEcho "Starting PHPCI Console Install." 2
+info "Starting PHPCI Console Install."
 php ./console phpci:install --url=http://$DEFAULT_DOMAIN --db-host=$DB_HOST --db-name=$DB_NAME --db-user=$DB_USER --db-pass=$DB_PASSWORD --admin-name=$1 --admin-pass=$2 --admin-mail=$3
 
 # Setup Cron
-coloredEcho "Setting up Cron Job!" 2
+info "Setting up Cron Job!"
 croncmd="php $(pwd)/console phpci:run-builds"
 cronjob="* * * * * $croncmd"
 cat <(fgrep -i -v "${croncmd}" <(crontab -l)) <(echo "${cronjob}") | crontab -
 
 # Provision
-coloredEcho "Provisioning PHPCI" 2
+info "Provisioning PHPCI"
 sudo bash /vagrant/scripts/serve.sh $DEFAULT_DOMAIN $(pwd)/public
 
-coloredEcho "*********************" 2
-coloredEcho "Install success." 2
-coloredEcho "You can access it at:" 2
-coloredEcho "http://${DEFAULT_DOMAIN}" 3
-coloredEcho "*********************" 2
-coloredEcho "Don't forget to add the domain to /etc/hosts file on your main machine!" 2
+info "*********************"
+info "Install success."
+info "You can access it at:"
+note "http://${DEFAULT_DOMAIN}"
+info "*********************"
+info "Don't forget to add the domain to /etc/hosts file on your main machine!"
